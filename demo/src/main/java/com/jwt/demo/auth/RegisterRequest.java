05c6b9f956b2d8bbc27e5e25ac17162c50ffa1f7
ricardogomez99 <ricardogomez0203@gmail.com>
2023-09-08 15:28:25 -0500

    proyectoSpringSecurityJWT

---
+++ demo/src/main/java/com/jwt/demo/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.jwt.demo.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    String username;
+    String password;
+    String firstname;
+    String lastname;
+    String country;
+    
+}
---
+++ demo/src/main/java/com/jwt/demo/user/Role.java
@@ -0,0 +1,6 @@
+package com.jwt.demo.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
